c783ab780184033ad9d102c201d2ab3c9c5c41f9
Bhavya Raj <rajbhavya1011@gmail.com>
2023-11-02 08:50:33 +0530

    Showing the Assignment

---
+++ Matrix.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+    //Task 1: print 2 matrices
+
+    int a[3][3], b[3][3];
+    printf("Entry of elements are Row wise\n");
+    printf("Enter 1st matrix\n");
+    for(int i=0; i<3; i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Enter 2nd matrix\n");
+    for(int i=0; i<3; i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&b[i][j]);
+        }
+        printf("\n");
+    }
+
+    //Task 2: addition of two matrices
+    printf("Addition of both matrices\n");
+    int sum;
+    for(int i=0; i<3; i++){
+        for(int j=0;j<3;j++){
+            sum = (a[i][j] + b[i][j]);
+            printf("%d ",sum);
+        }
+        printf("\n");
+    }
