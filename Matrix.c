918463615a4cfb94ec33b4fc203378f60a4d2d0c
rajbhavya1011 <rajbhavya1011@gmail.com>
2023-11-16 10:28:25 +0530

    updating assignment

--- Matrix.c
+++ Matrix.c
@@ -30,4 +30,62 @@ void main()
         }
         printf("\n");
     }
-//Task 3: Finding cofactor of a Matrix
+//Task 3: Finding adjoint of a Matrix
+int c[3][3];
+for(int i=0;i<3;i++)
+{
+    for(int j=0;j<3;j++)
+    {
+        if(i==0 && j==0)
+        c[i][j] = (a[1][1]*a[2][2]) - (a[1][2]*a[2][1]);
+         if(i==0 && j==1)
+        c[i][j] = -(a[1][0]*a[2][2]) - (a[1][2]*a[2][0]);
+         if(i==0 && j==2)
+        c[i][j] = (a[1][0]*a[2][1]) - (a[1][1]*a[2][0]);
+         if(i==1 && j==0)
+        c[i][j] = -(a[0][1]*a[2][2]) - (a[0][2]*a[2][1]);
+         if(i==1 && j==1)
+        c[i][j] = (a[0][0]*a[2][2]) - (a[0][2]*a[2][0]);
+         if(i==1 && j==2)
+        c[i][j] = -(a[0][0]*a[2][1]) - (a[0][1]*a[2][0]);
+         if(i==2 && j==0)
+        c[i][j] = (a[0][1]*a[1][2]) - (a[0][2]*a[1][1]);
+         if(i==2 && j==1)
+        c[i][j] = -(a[0][0]*a[1][2]) - (a[0][2]*a[1][0]);
+         if(i==2 && j==2)
+        c[i][j] = (a[0][0]*a[1][1]) - (a[0][1]*a[1][0]);
+    }
+}
+printf("Adjoint of 1st Matrix is:\n");
+for(int i=0;i<3;i++)
+{
+    for(int j=0;j<3;j++)
+    {
+        printf("%d ",c[i][j]);
+    }
+    printf("\n");
+}
+//Task 4 : Inverse of a matrix
+int det=0;
+    for(int j=0;j<3;j++)
+    {
+        if(j==0)
+        det = det + a[0][j]*((a[1][1]*a[2][2]) - (a[1][2]*a[2][1]));
+         if(j==1)
+        det = det - a[0][j]*((a[1][0]*a[2][2]) - (a[1][2]*a[2][0]));
+         if(j==2)
+        det = det + a[0][j]*((a[1][0]*a[2][1]) - (a[1][1]*a[2][0]));   
+    }
+    printf("Inverse of 1st Matrix is:\n");
+for(int i=0;i<3;i++)
+{
+    for(int j=0;j<3;j++)
+    {
+        printf("%d ",c[i][j]/det);
+    }
+    printf("\n");
+}
+}
+
+
+
